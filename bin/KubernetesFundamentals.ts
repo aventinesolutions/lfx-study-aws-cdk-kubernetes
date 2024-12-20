8e01db5247cfa67cf999d1e2d50a71a015589757
Matthew Evan Eichler <matthew.eichler@aventinesolutions.nl>
2024-12-20 12:35:09 +0100

    :bear: :construction: successful deployment of a VPC Stack
    202412181541 LFS258 Kubernetes Fundamentals

--- bin/KubernetesFundamentals.ts
+++ bin/KubernetesFundamentals.ts
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import { VpcStack } from '../lib/VpcStack';
 
 const app = new cdk.App();
+
+new VpcStack(app, 'LFXCDKVpcStack', {
+  vpcCidr: '10.100.0.0/20',
+  vpcAZs: ['eu-west-1a', 'eu-west-1b',],
+  ngAZs: ['eu-west-1a',],
+  publicCidrMask: 26,
+  privateCidrMask: 24,
+});
